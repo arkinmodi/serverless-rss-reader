3eca415da798d574440aab4be58a7649c079aa5e
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2023-08-15 22:41:34 -0400

    test to test tests are testing

---
+++ src/test/java/com/arkinmodi/rssreader/MainTests.java
@@ -0,0 +1,13 @@
+package com.arkinmodi.rssreader;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MainTests {
+
+  @Test
+  public void test() {
+    assertEquals(2, 2);
+  }
+}
